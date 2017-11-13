24436f53d0b113200eaa970bbff72af1f1d0c2a9
fzgharaei <gharaeemanesh@gmail.com>
2017-11-13 14:30:33 -0500

    writing parser... init commit

--- src/Client/ClientOptionParser.java
+++ src/Client/ClientOptionParser.java
@@ -1,7 +1,28 @@
 package Client;
 
+import java.util.HashMap;
+
 public class ClientOptionParser {
+	HashMap<String, String> keywords;
+	public ClientOptionParser(){
+		keywords = new HashMap<String,String>();
+		keywords.put("router-host", "localhost");
+		keywords.put("router-port", "3000");
+		keywords.put("server-host", "localhost");
+		keywords.put("server-port", "8007");
+	}
 	void parse(String[] args){
+		for(int i =0;i<args.length;i++){
+			switch(args[i]){
+			case "router-host":
+				keywords.put("router-host", args[i+1]);
+			case "router-port":
+			case "server-host":
+			case "server-port": 
+			default:
+				System.out.println("Bad input syntax");
+			}
+		}
 //		 parser.accepts("router-host", "Router hostname")
 //         .withOptionalArg()
 //         .defaultsTo("localhost");
