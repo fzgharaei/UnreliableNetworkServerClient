148d4a00954cb4685ae7ec7a958231f625701f13
rahulreddy94 <rahulmudupuri19944@gmail.com>
2017-11-15 10:47:21 -0500

    fixed parsers

--- src/Client/ClientOptionParser.java
+++ src/Client/ClientOptionParser.java
@@ -4,21 +4,36 @@ import java.util.HashMap;
 
 public class ClientOptionParser {
 	HashMap<String, String> keywords;
+	
 	public ClientOptionParser(){
 		keywords = new HashMap<String,String>();
-		keywords.put("router-host", "localhost");
-		keywords.put("router-port", "3000");
-		keywords.put("server-host", "localhost");
-		keywords.put("server-port", "8007");
+		//keywords.put("router-host", "localhost");
+		//keywords.put("router-port", "3000");
+		//keywords.put("server-host", "localhost");
+		//keywords.put("server-port", "8007");
 	}
+	
+	//router-host localhost router-port 3000 server-host localhost server-port 8007
 	void parse(String[] args){
 		for(int i =0;i<args.length;i++){
 			switch(args[i]){
 			case "router-host":
 				keywords.put("router-host", args[i+1]);
+				i++;
+				break;
 			case "router-port":
+				keywords.put("router-port", args[i+1]);
+				i++;
+				break;
 			case "server-host":
+				keywords.put("server-host", args[i+1]);
+				i++;
+				break;
 			case "server-port": 
+				keywords.put("server-port", args[i+1]);
+				i++;
+				break;
+
 			default:
 				System.out.println("Bad input syntax");
 			}
@@ -42,6 +57,6 @@ public class ClientOptionParser {
 	
 	String valueOf(String str){
 		// TO DO: make an actual value of
-		return str;
+		return keywords.get(str);
 	}
 }
--- src/Server/ServerOptionParser.java
+++ src/Server/ServerOptionParser.java
@@ -1,9 +1,34 @@
 package Server;
 
-import static java.util.Arrays.asList;
+import java.util.HashMap;
 
 public class ServerOptionParser {
+	
+	HashMap<String, String> serverKeywords;
+	
+	public ServerOptionParser() {
+
+		serverKeywords = new HashMap<String,String>();
+		//serverKeywords.put("port","8007");
+		
+	}
+	
 	void parse(String[] args){
+		
+		for(int i=0; i < args.length; i++){
+			switch (args[i]) {
+			
+			case "port":
+				serverKeywords.put("port","8007");
+				i++;
+				break;
+
+			default:
+				System.out.println("Bad input syntax");		
+				}
+		}
+		
+		
 //		parser.acceptsAll(asList("port", "p"), "Listening port")
 //        .withOptionalArg()
 //        .defaultsTo("8007");
@@ -11,6 +36,6 @@ public class ServerOptionParser {
 	
 	String valueOf(String str){
 		// TO DO: make an actual value of
-		return str;
+		return serverKeywords.get(str);
 	}
 }
